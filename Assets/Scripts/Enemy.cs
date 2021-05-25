d3ae8e22caa3d2cf92aff93d3248614d213f3c6b
Danylov <ol_danylov@ukr.net>
2021-05-25 16:57:44 +0300

    Add BOSS futeres.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,18 @@ public class Enemy : MonoBehaviour
     private Rigidbody enemyRb;
     private GameObject player;
     public float speed = 3.0f;
-    // Start is called before the first frame update
+    
+    public bool isBoss = false;
+    public float spawnInterval;
+    private float nextSpawn;
+    public int miniEnemySpawnCount;
+    private SpawnManager spawnManager;
+    
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+        if (isBoss)  spawnManager = FindObjectOfType<SpawnManager>();
     }
 
     // Update is called once per frame
@@ -19,6 +26,11 @@ public class Enemy : MonoBehaviour
     {
         Vector3  lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+        if (isBoss && (nextSpawn < Time.time))
+        {
+            nextSpawn = Time.time + spawnInterval;
+            spawnManager.SpawnMiniEnemy(miniEnemySpawnCount);
+        }
         if (transform.position.y < -10) Destroy(gameObject);
     }
 }
